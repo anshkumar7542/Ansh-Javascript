c547951014e46ae52f9da126a3a318afd48c28d1
Ansh kumar <anshsingh7542037689@gmail.com>
2025-11-05 23:15:45 +0530

    Initial commit

---
+++ 01_variable.js
@@ -0,0 +1,22 @@
+const account_Id=234523456
+let accountEmail ="anshsingh345@gmail.com";
+var accountPassword = "98765"
+accountCity = "patna"
+
+// account_Id =33 // not allowed
+accountEmail ="iuberi@gmail.com"
+accountPassword = "9876523456"
+accountCity = "york"
+let accountState;
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.log(account_Id);
+console.table([account_Id,accountEmail,accountPassword,accountCity,accountState]);
+
+
+
+
