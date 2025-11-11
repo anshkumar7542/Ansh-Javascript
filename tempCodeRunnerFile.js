491e6c61a97bd3cea391d1ac47bc59b9f3d42314
Ansh kumar <anshsingh7542037689@gmail.com>
2025-11-11 23:26:25 +0530

    initial commit

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,3 +1,17 @@
-let someNumber = 33
-// let stringNumber = String(someNumber)
-// console.log(typeof stringNumber);
+console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+
+// console.log("2">1);
+// console.log("02">1);
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+
+// console.log(undefined ==0);
+// console.log(undefined <0);
+// console.log(undefined >0);
