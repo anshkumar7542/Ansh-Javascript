aa2d385a1ab0842e9945e2eb6dccab01ccf55257
Ansh kumar <anshsingh7542037689@gmail.com>
2025-11-11 20:10:23 +0530

    datatypes

---
+++ 02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all js code as newer version or new js
+
+//alert(4+4)  we are using nodejs, not browser
+
+
+console.log(3+3)
+console.log("Ansh")
+
+let name ="ansh";
+let age =20;
+let isLoggedIn = false;
+
+//number => 2 to power 53
+//bigint use in (trading,reddit)
+//string =>"",''
+//boolean = true/false
+// null => standlone value
+//undefined => 
+// symbol=> unique
+//object
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
+
+
+
